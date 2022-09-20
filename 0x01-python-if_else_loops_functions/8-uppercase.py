5ddd07ca2806e3f2b2c4dc36ce869b545a90a158
francdomain <cfrancmail@gmail.com>
2022-09-20 11:38:48 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for ch in str:
         if ord(ch) >= 97 and ord(ch) <= 122:
             ch = ord(ch) - 32
-        print("{}".format(chr(ch)))
+        print("{}".format(chr(ch)), end="")
