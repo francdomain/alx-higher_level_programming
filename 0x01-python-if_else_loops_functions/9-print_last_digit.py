3fd120fb8ba39fbd6f28d7923d0a8afe5d74c07b
francdomain <cfrancmail@gmail.com>
2022-09-20 12:48:41 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,8 @@
 def print_last_digit(number):
     if number < 0:
         last_digit = number % -10
-        print("{}".format(last_digit))
-        return last_digit
+        print("{}".format(last_digit), end="")
     else:
         last_digit = number % 10
-        print("{}".format(last_digit))
-        return last_digit
+        print("{}".format(last_digit), end="")
+    return last_digit
