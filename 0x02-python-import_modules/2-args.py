c5cc1947788750a8430e575e0cb30ca185f0cd76
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-22 15:53:27 +0100

    dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    index = len(argv) - 1
+    if index == 0:
+        print("{} argument.".format(index))
+    elif index == 1:
+        print("{} argument:".format(index))
+    else:
+        print("{} arguments:".format(index))
+    for i in range(i + 1, argv[i + 1]):
+        print("{}: {}".format(index, argv[i + 1]))
