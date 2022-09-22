61489f03750cfc1622cfdf9906413ac173658e91
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-22 16:08:24 +0100

    dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("{} argument:".format(index))
     else:
         print("{} arguments:".format(index))
-    for i in range(i + 1, argv[i + 1]):
+    for i in range(index):
         print("{}: {}".format(i + 1, argv[i + 1]))
