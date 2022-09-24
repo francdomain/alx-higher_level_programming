73c130a02ecd978eec158eea4920d09e7675fca8
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-24 18:16:56 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,6 @@ def new_in_list(my_list, idix, element)i:
     elif idx > len(my_list) - 1:
         return new_list
     else:
-        if idx in new_list and element not in new_list:
+        if element not in new_list and idx < len(new_list) - 1:
             new_list[idx] = element
             return new_list
