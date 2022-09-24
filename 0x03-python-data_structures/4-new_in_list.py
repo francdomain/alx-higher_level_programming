06c4a55646426ff38f2b993fe6e9e21b54a91c41
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-24 19:14:32 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    idx <= len(my_list) - 1
     if idx < 0:
         retuen my_list
     elif idx > len(my_list) - 1:
         return my_list
     else:
         new_list = [elem for elem in my_list]
+        idx = len(new_list) - 1
         new_list[idx] = element
         return new_list
