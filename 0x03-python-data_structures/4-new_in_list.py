cddb2f525f1947c64ed68f7997fa15290055d023
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-24 17:21:48 +0100

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,4 +6,5 @@ def new_in_list(my_list, idx, element):
     elif idx > len(my_list) - 1:
         return my_list[:]
     else:
-        return my_list.replace(idx, element)
+        my_list.replace(idx, element)
+        return my_list
