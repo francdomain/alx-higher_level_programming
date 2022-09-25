9c518718b389afc5ff71105f1411cff60803d8ef
Francis Chukwuoma <cfrancmail@gmail.com>
2022-09-25 18:40:40 +0100

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,4 +2,12 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     x = tuple_a[0] + tuple_b[0]
     y = tuple_a[1] + tuple_b[1]
+    if len(tuple_a) < 2:
+        tuple_a[1] = 0
+    if len(tuple_b) < 2:
+        tuple_b[1] = 0
+    if len(tuple_a) > 2:
+        x = tuple_a[0] + tuple_b[0]
+    if len(tuple_b) > 2:
+        y = tuple_a[1] + tuple_b[1]
     print("{:d}, {:d}".format(x, y))
