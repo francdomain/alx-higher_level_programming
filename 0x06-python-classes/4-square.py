04b13b9ef273f2e8c4dbf9ec57b00d089633abb4
Francis Chukwuoma <cfrancmail@gmail.com>
2022-10-12 00:33:47 +0100

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,6 +23,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    """Display current square area"""
     def area(self):
+        """Return the area of the current square"""
         return self.__size * self.__size
