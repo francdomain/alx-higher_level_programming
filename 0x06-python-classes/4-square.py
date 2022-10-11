4b1bd86896065d9cc474f55f9484cd71cd9741bb
Francis Chukwuoma <cfrancmail@gmail.com>
2022-10-12 00:19:11 +0100

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Display a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square of attribute:
+            size (int): size of the square"""
+
+    @property
+    def size(self):
+        """Get the current square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set current square size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """Display current square area"""
+    def area(self):
+        """Return the area of the current square"""
+        return self.size * self.size
