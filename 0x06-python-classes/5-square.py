2015cc724ff75f86da35dd4052e27e4761b1c470
Francis Chukwuoma <cfrancmail@gmail.com>
2022-10-12 01:32:45 +0100

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Display a square"""
+
+    def __init__(self, size=0):
+        """Initialize a square of attribute:
+            size (int): size of the square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the current square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set current square size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the current square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """print the square with # in stdout"""
+        if self.__size == 0:
+            print()
+        for n in range(self.__size):
+            [print("#", end="") for m in range(self.__size)]
