f322dc063347a75d8198187e4db792b6cf4e99be
Francis Chukwuoma <cfrancmail@gmail.com>
2022-11-30 00:16:11 +0100

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT COUNT(*) AS `number` WHERE score NOT DISTINCT ORDER BY `number` DESC;
+SELECT COUNT(*) AS `number` WHERE score NOT DISTINCT GROUP BY `number` DESC;
