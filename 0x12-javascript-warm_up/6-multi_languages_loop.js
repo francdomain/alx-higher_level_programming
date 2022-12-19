095d85028ba45753125703c17a43761c4e987171
Francis Chukwuoma <cfrancmail@gmail.com>
2022-12-19 20:55:34 +0100

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,3 @@
-const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const line of arr) {
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(line);
 }
