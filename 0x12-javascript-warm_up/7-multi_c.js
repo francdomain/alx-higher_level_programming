9f0042160f9b96e771808ff4b010a7ac09c946a6
Francis Chukwuoma <cfrancmail@gmail.com>
2022-12-19 23:26:44 +0100

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,6 @@
+const x = process.argv[2];
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  console.log('C is fun' * x);
+}
