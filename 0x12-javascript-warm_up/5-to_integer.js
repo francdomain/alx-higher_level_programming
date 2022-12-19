7a50c6e2f6a819beee39e5433d1fd4af9a408094
Francis Chukwuoma <cfrancmail@gmail.com>
2022-12-19 21:00:16 +0100

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('My number: ', Number(process.argv[2]));
+  console.log('My number: ' + Number(process.argv[2]));
 } else {
   console.log('Not a number');
 }
